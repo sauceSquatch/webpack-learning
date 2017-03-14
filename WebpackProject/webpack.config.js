0c62213386c31862adbeb3f67d0b33e455e9e0a9
Jamie Naioti <naioti@gmail.com>
2017-03-14 14:22:14 -0500

    edits to code split vendor.js with bundle.js

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -1,11 +1,28 @@
 var webpack = require('webpack');
 var path = require('path');
 
+
+const VENDOR_LIBS = [
+  "react",
+  "faker",
+  "lodash",
+  "react-dom",
+  "react-input-range",
+  "react-redux",
+  "react-router",
+  "redux",
+  "redux-form",
+  "redux-thunk"
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
